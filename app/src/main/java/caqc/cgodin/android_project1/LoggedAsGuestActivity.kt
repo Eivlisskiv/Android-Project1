49d8b9767d90d9775d8da77a860d0d331c2536c4
ShrekTheMovie <philemonleger@gmail.com>
2020-11-10 13:27:12 -0500

    logged as guest activity creation

---
+++ app/src/main/java/caqc/cgodin/android_project1/LoggedAsGuestActivity.kt
@@ -0,0 +1,11 @@
+package caqc.cgodin.android_project1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoggedAsGuestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_logged_as_guest)
+    }
+}
--- app/src/main/java/caqc/cgodin/android_project1/MainActivity.kt
+++ app/src/main/java/caqc/cgodin/android_project1/MainActivity.kt
@@ -1,5 +1,6 @@
 package caqc.cgodin.android_project1
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
@@ -10,12 +11,13 @@ class MainActivity() : ActivityExtension() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main, arrayOf(
-            R.id.login_email_label, R.id.login_password_label,
-            R.id.login_login_button
+        R.id.login_email_label, R.id.login_password_label,
+        R.id.login_login_button, R.id.login_login_as_guest_button
         ))
     }
 
     fun login_button(view: View?){
-        this.updateViewLanguage()
+        val intent = Intent(this@MainActivity, LoggedAsGuestActivity::class.java)
+        startActivity(intent)
     }
 }
